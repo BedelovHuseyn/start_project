565a49fdb38d3c6de9463897b2427681c2aaca31
Bedelov <huseyn@creator.az>
2022-04-19 22:38:37 +0400

    project init

--- 19.04.2022.py
+++ 19.04.2022.py
@@ -1,3 +1,23 @@
-# salam
-# salam2
-#salam3
+# a = float(input('Kvadrati hesablanacaq reqemi daxil edin: '))
+
+
+# str soz dizisidir
+# a = 7 --->int tam ededlerdir
+# a = 13.7  ---> float kəsr ədədlərə deyilir
+# b = 14.5  ----> double floatin basqa adı
+# a = 'Gorushdukde ilk sozu "salam", oldu'
+# (3<5)-->{True} boolen cavab olaraq True ve Folse deyerini dondurur
+# a = ["str","int","input"] ---->array basqa adi ile liste
+##   ###   ####
+
+# diller = {"php":25,"Java":30,"SQL":15}   #------>dictionary
+#diller["Python"] = 35   elave etmek ucun isdifade olunur
+#diller.clear()  ----> butun dictionary(sozluk) silinir
+#diller.pop("php") ---> secilen dili silir
+#diller.popitem() ----> rastgele birini siler
+#diller2 = diller.copy() ----> yeni sozluk yaradar ve isdediyin sozluyu kopyalayar
+# key = 'SQL'
+# print(diller[key])
+# a = None
+
+# print(type(a))
