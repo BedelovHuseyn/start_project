673983d5f1a3a7f4027c93fd0bfdea3852f96f29
Badalov <huseyn@creator.az>
2022-05-11 23:52:12 +0400

    git init

---
+++ while.py
@@ -0,0 +1,10 @@
+# Loop-lar ya bir elementi dondurur, ya da bir shert esasinda donur
+# while loopu if kimi ishleyir ve daxiline yazilmish shertden True qayitdigi ana qeder donur
+# Meselen
+# a = 6
+# print(a<10) bu kod True qaytarir
+# while(a<10):
+#     print(a)
+#     a+=1
+# Bu kod 10-e qeder 10 daxil olmamaq serti ile ekrana 6,7,8,9 ededlerini yazir ve kod sonlanir!
+# print(a<10) bu kod loop bitdikde deyeri 10-a beraber oldugu ucun False qaytarir
