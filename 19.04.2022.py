070f55a7c153376f5dfbdb8dcad9581c5ef3ced6
Badalov <huseyn@creator.az>
2022-05-14 08:15:56 +0400

    git init

--- 19.04.2022.py
+++ 19.04.2022.py
@@ -118,4 +118,17 @@ print("Sayilar" , e)
 f = random.sample(sayilar, 3)     #  verilen say qeder regem verir
 print(f)    '''
 
+# Loop-lar ya bir elementi dondurur, ya da bir shert esasinda donur
+# while loopu if kimi ishleyir ve daxiline yazilmish shertden True qayitdigi ana qeder donur
+# Meselen
+# a = 6
+# print(a<10) bu kod True qaytarir
+# while(a<10):
+#     print(a)
+#     a+=1
+# Bu kod 10-e qeder 10 daxil olmamaq serti ile ekrana 6,7,8,9 ededlerini yazir ve kod sonlanir!
+# print(a<10) bu kod loop bitdikde deyeri 10-a beraber oldugu ucun False qaytarir
+
+
+# For - loopu arrayin icinde olan verileri tek tek oxuyur sonra yeni bir  deyisen icinde yazdirir
 
--- while.py
+++ while.py
@@ -8,3 +8,6 @@
 #     a+=1
 # Bu kod 10-e qeder 10 daxil olmamaq serti ile ekrana 6,7,8,9 ededlerini yazir ve kod sonlanir!
 # print(a<10) bu kod loop bitdikde deyeri 10-a beraber oldugu ucun False qaytarir
+
+
+# For - loopu arrayin icinde olan verileri tek tek oxuyur sonra yeni bir  deyisen icinde yazdirir
